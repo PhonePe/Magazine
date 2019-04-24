4e8e60151cb1629e36ad30adc0b26e95fdd9c9b4
nishant141077 <nishant.gupta@phonepe.com>
2019-04-24 18:12:34 +0530

    made some fields public

--- src/main/java/com/phonepe/growth/magazine/core/MagazineType.java
+++ src/main/java/com/phonepe/growth/magazine/core/MagazineType.java
@@ -6,8 +6,8 @@ public enum MagazineType {
     AEROSPIKE(MagazineType.AEROSPIKE_TEXT),
     HBASE(MagazineType.HBASE_TEXT);
 
-    static final String AEROSPIKE_TEXT = "AEROSPIKE";
-    static final String HBASE_TEXT = "HBASE";
+    public static final String AEROSPIKE_TEXT = "AEROSPIKE";
+    public static final String HBASE_TEXT = "HBASE";
 
     @Getter
     private final String value;
