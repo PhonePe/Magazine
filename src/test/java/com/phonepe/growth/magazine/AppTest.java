9b035264f3f63a34b84afc98c61c4c7676488a92
Ravneet Baansal <ravneet.baansal@PP-C02WK0DFG8WN-Ravneet.local>
2020-06-04 12:03:16 +0530

    restructured test package

--- src/test/java/com/phonepe/growth/magazine/AppTest.java
+++ src/test/java/com/phonepe/growth/magazine/AppTest.java
@@ -1,4 +1,4 @@
-package com.phonepe.growth;
+package com.phonepe.growth.magazine;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
     extends TestCase
 {
     /**
